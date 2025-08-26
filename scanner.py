493ec17f160ac3396f2a2fff9ffada54343d8af7
AkiraWake <akiramartins666@gmail.com>
2025-08-26 17:25:38 -0300

    Adiciona JSON para subdomínios sensíveis

--- scanner.py
+++ scanner.py
@@ -1,4 +1,5 @@
 import json
+import re
 import sys
 import dns.resolver
 import socket
@@ -26,6 +27,18 @@ resolver = dns.resolver.Resolver()
 resolver.timeout = 2    # Tempo máximo de resposta por tentativa
 resolver.lifetime = 2   # Tempo máximo de vida para a consulta
 tentativas = 2          # Número de tentativas em caso de timeout
+
+sensiveis = [    "admin","portal","intranet","extranet","secure","sso","vpn","access","internal",
+    "private","files","ftp","db","database","backup","test","dev","stage","staging",
+    "git","svn","repo","config","dashboard","panel","monitor","nagios","zabbix",
+    "reports","logs","old","legacy","archive"
+]
+
+
+def sensivel(subdomain: str)-> bool:
+    tokens = re.split(r"[.\-]", subdomain.lower())
+    return any(tok in sensiveis for tok in tokens)
+
 
 # Função que verifica se um subdomínio existe
 def verificar_subdomain(d):
@@ -100,6 +113,20 @@ with ThreadPoolExecutor(max_workers=20) as executor:
             # Armazena no array final
             resultados.append(resultado)
 
+# --- Filtra os subdomínios que foi classificados como sensiveis ---
+print("==Subdomínios sensíveis Encontrados==")
+resultados_sensiveis = []
+
+for r in resultado:
+    if sensivel(r["subdomain"]):
+         print(f"[CRÍTICO] {r['subdomain']} - IPv4: {r['ipv4']} - IPv6: {r['ipv6']} - CNAME: {r['cname']}")
+         resultados_sensiveis.append(r)
+
+
 # --- Salva os resultados em arquivo JSON ---
 with open("Resultado.json", "w") as f:
     json.dump(resultados, f, indent=4)
+
+# --- Salva os resultados sensiveis em arquivo JSON ---
+with open("Sensiveis.json", "w") as f:
+    json.dump(resultados_sensiveis, f, indent=4)
